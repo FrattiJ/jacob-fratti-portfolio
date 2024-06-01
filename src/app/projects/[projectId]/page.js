ead196c67faa76c564d5803a15911add6d8765d8
Jacob Fratti <JacobRFratti@gmail.com>
2024-06-01 18:46:19 -0400

    Set up routes

---
+++ src/app/projects/[projectId]/page.js
@@ -0,0 +1,10 @@
+export default function ProjectDetails({ params }) {
+    const { projectId } = params;
+  
+    return (
+      <main>
+        <h1> Project {projectId} Details </h1>
+      </main>
+    );
+  }
+  
